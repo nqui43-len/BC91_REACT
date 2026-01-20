e46f9e06e4fe853288725f09e79eb2b031b5610e
unknown <nqui43@gmail.com>
2026-01-20 19:36:09 +0700

    first commit

---
+++ reactbc91/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <h1>Hello cybersoft</h1>
+    <img src='/vite.svg' alt='111' />
+  </>
+)
+
